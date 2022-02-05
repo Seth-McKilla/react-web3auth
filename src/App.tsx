6cb86cec54fa00e90f21244a3c23877ff1d1b2e6
Seth McCullough <seth@endevrs.dev>
2022-02-04 21:33:56 -0500

    Remove boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <h2>React Web3Auth</h2>
+        <button>Login</button>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Seth-McKilla/react-web3auth"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check it out on GitHub!
         </a>
       </header>
     </div>
